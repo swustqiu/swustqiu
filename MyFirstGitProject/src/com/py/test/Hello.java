888074575cf0ada53a094dbe5b5f446942291270
qiuting <swustqiu@126.com>
2018-11-29 15:22:10 +0800

    提交测试

---
+++ MyFirstGitProject/src/com/py/test/Hello.java
@@ -0,0 +1,14 @@
+package com.py.test;
+
+/**
+ * 
+ * @author qiuting
+ * @date 2018年11月29日 下午3:07:25
+ *
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println(System.currentTimeMillis());
+	}
+}
