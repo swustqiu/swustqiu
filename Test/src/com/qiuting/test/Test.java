70c2a4a269ba5d3ee2254df9fc324d7cf7044d5d
qiuting <swustqiu@126.com>
2018-11-29 15:14:31 +0800

    第一次提交

---
+++ Test/src/com/qiuting/test/Test.java
@@ -0,0 +1,14 @@
+package com.qiuting.test;
+
+/**
+ * 
+ * @author qiuting
+ * @date 2018年11月29日 下午2:52:25
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("end");
+	}
+}
